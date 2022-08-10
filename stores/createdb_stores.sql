4609622545bcd3312684d0bce26f21f932f77d4f
camiladaltro <92816266+camiladaltro@users.noreply.github.com>
2022-08-10 12:22:48 -0300

    Add files via upload

---
+++ stores/createdb_stores.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE stores (
+  Store_Id INT,
+  Store_Area INT,
+  Items_Available INT,
+  Daily_Customer_Count INT,
+  Store_Sales INT
+);
+
+copy Stores FROM '/home/oem/Downloads/Stores.csv' DELIMITER ',' CSV HEADER;
